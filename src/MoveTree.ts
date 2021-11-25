6771401c50adefb6598471ef2c516df178ec332a
Benjamin Jones <benjaminpaikjones@gmail.com>
2021-11-25 16:02:07 -0500

    Allow black or white to have a string value, in the case where multiple points are being set.

--- src/MoveTree.ts
+++ src/MoveTree.ts
@@ -36,8 +36,8 @@ export interface MarkInterface {
     stone_removed?    : boolean;
     mark_x?           : boolean;
     hint?             : boolean;
-    black?            : boolean;
-    white?            : boolean;
+    black?            : boolean | string;
+    white?            : boolean | string;
     color?            : string;
 
     [label: string]: string | boolean | undefined;
