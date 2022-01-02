23d66d4dd71c4ab0d113c6ed18bfc15d0740ecad
GreenAsJade <greenasjade@gregories.net>
2022-01-02 19:44:20 +1030

    version string update (lets put this in About) and undebug

--- src/GobanCanvas.ts
+++ src/GobanCanvas.ts
@@ -178,7 +178,7 @@ export class GobanCanvas extends GobanCore  {
 
         this.ready_to_draw = true;
         this.redraw(true);
-        console.log("Goban Canvas v 0.5.66");
+        console.log("Goban Canvas v 0.5.68");
     }
     public enablePen():void {
         this.attachPenCanvas();
