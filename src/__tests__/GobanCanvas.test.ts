b3ebb95337596590ad05ad852693068f2cb99078
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-11-19 07:40:51 -0800

    Add tests for a couple cases where clicking should have no effect.
        - stone placement not enabled
        - clicking on the midpoint of two intersections.

--- src/__tests__/GobanCanvas.test.ts
+++ src/__tests__/GobanCanvas.test.ts
@@ -6,10 +6,42 @@
 
 import { GobanCanvas } from "../GobanCanvas";
 
+let board_div: HTMLDivElement;
+
 describe("onTap", () => {
-    test("clicking the top left intersection places a stone", () => {
-        const board_div = document.createElement("div");
+    beforeEach(() => {
+        board_div = document.createElement("div");
         document.body.appendChild(board_div);
+    });
+
+    afterEach(() => {
+        board_div.remove();
+    });
+
+    test("clicking without enabling stone placement has no effect", () => {
+        const goban = new GobanCanvas({
+            width: 3,
+            height: 3,
+            square_size: 10,
+            board_div: board_div,
+            interactive: true,
+        });
+        const canvas = document.getElementById("board-canvas") as HTMLCanvasElement;
+        const mouse_event = new MouseEvent("click", {
+            clientX: 15,
+            clientY: 15,
+        });
+
+        canvas.dispatchEvent(mouse_event);
+
+        expect(goban.engine.board).toEqual([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]);
+    });
+
+    test("clicking the top left intersection places a stone", () => {
         const goban = new GobanCanvas({
             width: 3,
             height: 3,
@@ -28,6 +60,54 @@ describe("onTap", () => {
 
         expect(goban.engine.board).toEqual([
             [1, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]);
+    });
+
+    test("clicking the midpoint of two intersections has no effect", () => {
+        const goban = new GobanCanvas({
+            width: 3,
+            height: 3,
+            square_size: 10,
+            board_div: board_div,
+            interactive: true,
+        });
+        const canvas = document.getElementById("board-canvas") as HTMLCanvasElement;
+        const mouse_event = new MouseEvent("click", {
+            clientX: 20,
+            clientY: 15,
+        });
+
+        goban.enableStonePlacement();
+        canvas.dispatchEvent(mouse_event);
+
+        expect(goban.engine.board).toEqual([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]);
+    });
+
+    test("clicking the midpoint of two intersections has no effect", () => {
+        const goban = new GobanCanvas({
+            width: 3,
+            height: 3,
+            square_size: 10,
+            board_div: board_div,
+            interactive: true,
+        });
+        const canvas = document.getElementById("board-canvas") as HTMLCanvasElement;
+        const mouse_event = new MouseEvent("click", {
+            clientX: 20,
+            clientY: 15,
+        });
+
+        goban.enableStonePlacement();
+        canvas.dispatchEvent(mouse_event);
+
+        expect(goban.engine.board).toEqual([
+            [0, 0, 0],
             [0, 0, 0],
             [0, 0, 0],
         ]);
