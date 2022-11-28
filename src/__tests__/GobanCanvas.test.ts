3ffacb4339e7875099aa45b0e495ef3567ae2b02
Benjamin Jones <benjaminpaikjones@gmail.com>
2022-11-27 21:31:02 -0800

    Add test for analysis label subtool.

--- src/__tests__/GobanCanvas.test.ts
+++ src/__tests__/GobanCanvas.test.ts
@@ -8,6 +8,23 @@ import { GobanCanvas } from "../GobanCanvas";
 
 let board_div: HTMLDivElement;
 
+// Nothing special about this square size, just easy to do mental math with
+const TEST_SQUARE_SIZE = 10;
+function simulateMouseClick(canvas: HTMLCanvasElement, { x, y }: { x: number; y: number }) {
+    const eventInitDict = {
+        // 1.5 assumes axis labels, which take up exactly one stone width
+        clientX: (x + 1.5) * TEST_SQUARE_SIZE,
+        clientY: (y + 1.5) * TEST_SQUARE_SIZE,
+    } as const;
+
+    // Some actions are triggered on 'mousedown', others on 'click'
+    // As far as the onTap tests are concerned, it doesn't matter which, so we
+    // trigger all three mouse events when simulating the mouse click.
+    canvas.dispatchEvent(new MouseEvent("mousedown", eventInitDict));
+    canvas.dispatchEvent(new MouseEvent("mouseup", eventInitDict));
+    canvas.dispatchEvent(new MouseEvent("click", eventInitDict));
+}
+
 describe("onTap", () => {
     beforeEach(() => {
         board_div = document.createElement("div");
@@ -27,12 +44,8 @@ describe("onTap", () => {
             interactive: true,
         });
         const canvas = document.getElementById("board-canvas") as HTMLCanvasElement;
-        const mouse_event = new MouseEvent("click", {
-            clientX: 15,
-            clientY: 15,
-        });
 
-        canvas.dispatchEvent(mouse_event);
+        simulateMouseClick(canvas, { x: 0, y: 0 });
 
         expect(goban.engine.board).toEqual([
             [0, 0, 0],
@@ -50,13 +63,9 @@ describe("onTap", () => {
             interactive: true,
         });
         const canvas = document.getElementById("board-canvas") as HTMLCanvasElement;
-        const mouse_event = new MouseEvent("click", {
-            clientX: 15,
-            clientY: 15,
-        });
 
         goban.enableStonePlacement();
-        canvas.dispatchEvent(mouse_event);
+        simulateMouseClick(canvas, { x: 0, y: 0 });
 
         expect(goban.engine.board).toEqual([
             [1, 0, 0],
@@ -74,37 +83,9 @@ describe("onTap", () => {
             interactive: true,
         });
         const canvas = document.getElementById("board-canvas") as HTMLCanvasElement;
-        const mouse_event = new MouseEvent("click", {
-            clientX: 20,
-            clientY: 15,
-        });
 
         goban.enableStonePlacement();
-        canvas.dispatchEvent(mouse_event);
-
-        expect(goban.engine.board).toEqual([
-            [0, 0, 0],
-            [0, 0, 0],
-            [0, 0, 0],
-        ]);
-    });
-
-    test("clicking the midpoint of two intersections has no effect", () => {
-        const goban = new GobanCanvas({
-            width: 3,
-            height: 3,
-            square_size: 10,
-            board_div: board_div,
-            interactive: true,
-        });
-        const canvas = document.getElementById("board-canvas") as HTMLCanvasElement;
-        const mouse_event = new MouseEvent("click", {
-            clientX: 20,
-            clientY: 15,
-        });
-
-        goban.enableStonePlacement();
-        canvas.dispatchEvent(mouse_event);
+        simulateMouseClick(canvas, { x: 0.5, y: 0 });
 
         expect(goban.engine.board).toEqual([
             [0, 0, 0],
@@ -150,5 +131,28 @@ describe("onTap", () => {
             [0, 0, 0],
         ]);
         expect(goban.engine.cur_move.move_number).toBe(2);
+    });
+
+    test("Clicking with the triangle subtool places a triangle", () => {
+        const goban = new GobanCanvas({
+            width: 3,
+            height: 3,
+            square_size: 10,
+            board_div: board_div,
+            interactive: true,
+            mode: "analyze",
+        });
+        const canvas = document.getElementById("board-canvas") as HTMLCanvasElement;
+
+        goban.enableStonePlacement();
+        goban.setAnalyzeTool("label", "triangle");
+        simulateMouseClick(canvas, { x: 0, y: 0 });
+
+        expect(goban.getMarks(0, 0)).toEqual({ triangle: true });
+        expect(goban.engine.board).toEqual([
+            [0, 0, 0],
+            [0, 0, 0],
+            [0, 0, 0],
+        ]);
     });
 });
