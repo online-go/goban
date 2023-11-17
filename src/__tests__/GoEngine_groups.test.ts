5acdc1ac33170359d630fbeaf7c4313b4f0a7511
Benjamin P. Jones <benjaminpaikjones@gmail.com>
2023-11-16 20:22:26 -0800

    Add GoEngine groups test.

---
+++ src/__tests__/GoEngine_groups.test.ts
@@ -0,0 +1,56 @@
+import { GoEngine } from "../GoEngine";
+import { makeMatrix } from "../GoMath";
+
+test("toggleMetagroupRemoval", () => {
+    const engine = new GoEngine({
+        width: 4,
+        height: 4,
+        initial_state: { black: "aabbdd", white: "cacbcccd" },
+    });
+
+    /*   A B C D
+     * 4 x . o .
+     * 3 . x o .
+     * 2 . . o .
+     * 1 . . o x
+     */
+
+    const on_removal_updated = jest.fn();
+    engine.addListener("stone-removal.updated", on_removal_updated);
+
+    engine.toggleMetaGroupRemoval(0, 0);
+
+    expect(on_removal_updated).toBeCalledTimes(1);
+
+    expect(engine.removal).toEqual([
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 0, 0],
+        [0, 0, 0, 0],
+    ]);
+
+    engine.toggleMetaGroupRemoval(0, 0);
+
+    expect(engine.removal).toEqual(makeMatrix(4, 4));
+});
+
+test("toggleMetagroupRemoval out-of-bounds", () => {
+    const engine = new GoEngine({
+        width: 4,
+        height: 4,
+        initial_state: { black: "aabbdd", white: "cacbcccd" },
+    });
+
+    /*   A B C D
+     * 4 x . o .
+     * 3 . x o .
+     * 2 . . o .
+     * 1 . . o x
+     */
+
+    const on_removal_updated = jest.fn();
+    engine.addListener("stone-removal.updated", on_removal_updated);
+
+    expect(engine.toggleMetaGroupRemoval(0, 4)).toEqual([[0, []]]);
+    expect(on_removal_updated).toBeCalledTimes(0);
+});
