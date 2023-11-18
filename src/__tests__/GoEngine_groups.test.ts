2d4511bb0dd4074d4a12d706f8c417aaa4785d65
Benjamin P. Jones <benjaminpaikjones@gmail.com>
2023-11-18 14:16:23 -0800

    Add toggle empty area test.

--- src/__tests__/GoEngine_groups.test.ts
+++ src/__tests__/GoEngine_groups.test.ts
@@ -54,3 +54,27 @@ test("toggleMetagroupRemoval out-of-bounds", () => {
     expect(engine.toggleMetaGroupRemoval(0, 4)).toEqual([[0, []]]);
     expect(on_removal_updated).toBeCalledTimes(0);
 });
+
+test("toggleMetagroupRemoval empty area", () => {
+    const engine = new GoEngine({
+        width: 4,
+        height: 2,
+        initial_state: { black: "aabb", white: "cacb" },
+    });
+
+    /*   A B C D
+     * 4 x . o .
+     * 3 . x o .
+     * 2 . . o .
+     * 1 . . o x
+     */
+
+    const on_removal_updated = jest.fn();
+    engine.addListener("stone-removal.updated", on_removal_updated);
+
+    expect(engine.toggleMetaGroupRemoval(0, 1)).toEqual([
+        [1, [{ x: 0, y: 1 }]],
+        [0, []],
+    ]);
+    expect(on_removal_updated).toBeCalledTimes(1);
+});
